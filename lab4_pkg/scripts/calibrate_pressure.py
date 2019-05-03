4fc4e50a433f7d74ea8c698e57aba8bfb8aab110
Hasith <hrajaka@gmail.com>
2019-05-02 17:03:12 -0700

    updated pressure cailbration

--- lab4_pkg/scripts/calibrate_pressure.py
+++ lab4_pkg/scripts/calibrate_pressure.py
@@ -4,11 +4,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import sys
 
+ROOM_PRESSURE = 14.6959 # room pressure in psi
 
 def calibrate_pressure(adc_val):
-    Pmin    = 14.5038  # minimum pressure in psi
-    Pmax    = 114.5038 # maximum pressure in psi
-    Vsupply = 5.0       # supply voltage
+    Pmin    = ROOM_PRESSURE # minimum pressure in psi
+    Pmax    = Pmin + 100    # maximum pressure in psi
+    Vsupply = 5.0           # supply voltage
 
     Vout = float(adc_val) * 5.0 / 1023.0
 
@@ -18,14 +19,14 @@ def calibrate_pressure(adc_val):
 
     
 
-filename = '../data/calibration/flex_100.csv'
+filename = '../data/calibration/flex_200.csv'
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
 
 df = pd.read_csv(filename)
 
-pressure = df['right_pressure'].apply(calibrate_pressure)
+pressure = df['left_pressure'].apply(calibrate_pressure)
 
 
 plt.figure()
@@ -34,7 +35,7 @@ plt.title(filename)
 plt.axvline(color='k')
 plt.axhline(color='k')
 plt.plot(df['time'], df['left_pwm'], label='left_pwm')
-plt.plot(df['time'], df['right_pressure'], label='left_pressure')
+plt.plot(df['time'], df['left_pressure'], label='left_pressure')
 plt.plot(df['time'], df['left_flex'], label='left_flex')
 plt.plot(df['time'], df['tip_pos_x'] - df['base_pos_x'], label='pos_x')
 plt.plot(df['time'], df['tip_pos_y'] - df['base_pos_y'], label='pos_y')
